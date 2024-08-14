b6c371b9a4b4e7af348e52b2e5402ea776ad5982
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-08-14 09:13:27 +0200

    Form looking good

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,28 +1,52 @@
-import React, { useReducer, useContext } from 'react'
+import React, { cloneElement, useContext } from 'react'
 import { TodosContext } from '../context/todos'
 
 export default function TodoForm() {
+  const {
+    createNewTodo,
+    todoLabel,
+    todoIsCompleted,
+    onLabelChange,
+    onIsCompletedChange
+  } = useContext(TodosContext)
+
+  const resetForm = () => {
+    onLabelChange('')
+    onIsCompletedChange(false)
+  }
+
+  const onNewTodo = evt => {
+    evt.preventDefault()
+    createNewTodo(todoLabel, todoIsCompleted)
+    resetForm()
+    console.log({todoLabel},{todoIsCompleted})
+  }
 
   return (
-    <form id="todoForm">
+    <form id="todoForm" onSubmit={onNewTodo}>
       <h3>Create New Todo Form</h3>
       <label><span>Todo label:</span>
         <input
           type='text'
           name='todoLabel'
           placeholder='Type label'
+          onChange={e => onLabelChange(e.target.value)}
+          value={todoLabel}
         />
       </label>
       <label><span>Is completed:</span>
         <input
           type='checkbox'
           name='todoIsCompleted'
+          onChange={e => onIsCompletedChange(e.target.checked)}
+          checked={todoIsCompleted}
         />
       </label>
       <label><span>Create todo:</span>
         <input
           type='submit'
           value='Do it!'
+          disabled={!todoLabel.trim()}
         />
       </label>
     </form>
--- src/context/todos.jsx
+++ src/context/todos.jsx
@@ -2,6 +2,10 @@ import React, { createContext, useReducer } from "react";
 
 export const TodosContext = createContext()
 
+const ADD_NEW_TODO = 'ADD_NEW_TODO'
+const CHANGE_LABEL = 'CHANGE_LABEL'
+const CHANGE_IS_COMPLETED = 'CHANGE_IS_COMPLETED'
+
 let id = 1
 const getNextId = () => id++
 const initialState = {
@@ -11,12 +15,51 @@ const initialState = {
         { id: getNextId(), label: 'Groceries', complete: false },
         { id: getNextId(), label: 'Dishes', complete: false },
     ],
+    todoLabel: '',
+    todoIsCompleted: false,
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ADD_NEW_TODO: {
+            return { ...state, todos: [...state.todos, action.payload] }
+        }
+        case CHANGE_LABEL: {
+            return { ...state, todoLabel: action.payload }
+        }
+        case CHANGE_IS_COMPLETED: {
+            return { ...state, todoIsCompleted: action.payload }
+        }
+        default:
+            return state
+    }
 }
 
 export function TodosProvider(props) {
-    
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const createNewTodo = (label, complete) => {
+        const newTodo = { id: getNextId(), label, complete }
+        dispatch({ type: ADD_NEW_TODO, payload: newTodo })
+    }
+
+    const onLabelChange = (value) => {
+        dispatch({ type: CHANGE_LABEL, payload: value })
+    }
+
+    const onIsCompletedChange = (checked) => {
+        dispatch({ type: CHANGE_IS_COMPLETED, payload: checked })
+    }
+
+    const dataToProvide = {
+        createNewTodo,
+        onLabelChange,
+        onIsCompletedChange,
+        ...state
+    }
+
     return (
-        <TodosContext.Provider>
+        <TodosContext.Provider value={dataToProvide}>
             {props.children}
         </TodosContext.Provider>
     )
